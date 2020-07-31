8624de6625f5df1ab52bd9569949072a23d87f8f
SAR-SA <shawn.a.rather@gmail.com>
2020-07-30 21:14:58 -0400

    Local Storage fixed

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    console.log(localStorage.getItem('9AM'))
 
     const todayDate = moment();
     const currentDayElement = $('#currentDay');
@@ -9,28 +10,58 @@ $(document).ready(function () {
     //Header Date - using moment.js
     currentDayElement.text(todayDate.format('dddd, MMMM DD YYYY'));
 
-    
-    for (let i = 9; i < 18; i++) {
+
+    //Function to save To Do's
+    function savebutton() {
         
+        // button.addEventListener('click', storeTodo);
+        $('.saveBtn').on('click', storeTodo)
+    }   
+
+    
+
+    var todo = 'textarea'
+
+    function storeTodo() {
+        console.log("THIS BUTTON IS WORKING");
+        $(this).siblings('.storage').val()
+        // console.log(
+        //     $(this) // whatveer is setting off this event
+        //     .siblings('.hour') // another element that is a child of THIS's parent element
+        //     .text() // a method that returns the inner text of the sibling with "hour" class
+        //     )
+        localStorage.setItem($(this).siblings('.hour').text(), $(this).siblings('.storage').val());
+    }
+
+    for (let i = 9; i < 18; i++) {
+
         const rowEl = $('<div>');
         const hourEl = $('<div>');
         const calTextArea = $('<textarea>');
         const saveBtn = $('<button>');
         const currentHour = moment().hour();
         const storageInput = document.querySelector('.storage')
-        
+
         rowEl.addClass('row time-block');
         hourEl.addClass('col-1 hour');
         hourEl.text(i + "AM");
         calTextArea.addClass('col-10 storage');
+
+        // if i < 12
+        calTextArea.text(localStorage.getItem(i + "AM"));
+        // else  if i == 12
+        // calTextArea.text(localStorage.getItem((i  + "PM"))
+        // else 
+        // calTextArea.text(localStorage.getItem((i - 12) + "PM"))
+        
         calTextArea.attr('input');
         saveBtn.addClass('col-1 saveBtn');
         saveBtn.attr('type', 'button');
         saveBtn.html('<i class="far fa-save"></i>');
-        
+
         container.append(rowEl);
         rowEl.append(hourEl, calTextArea, saveBtn);
-        
+
         function addToDos() {
             storageInput.addEventListener('text', letter => {
                 console.log(letter.target.value)
@@ -38,7 +69,7 @@ $(document).ready(function () {
             })
         }
 
-        if (i > 12) {
+        if (i > 11) {
             hourEl.text(i - 12 + "PM");
         }
 
@@ -56,6 +87,8 @@ $(document).ready(function () {
     }
     savebutton();
     addToDos();
+
+
 });
 
 // const input = document.querySelector('input')
@@ -69,16 +102,5 @@ $(document).ready(function () {
 
 
 
-//Function to save To Do's
-function savebutton() {
-    const button = document.querySelector('.saveBtn');
-    button.addEventListener('click', storeTodo);
-}
 
-
-var todo = 'textarea'
-
-function storeTodo() {
-    localStorage.setItem('text', todo)
-}
 
